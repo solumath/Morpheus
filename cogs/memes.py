c3b0316227db009d86ffe0d6eb48dd9fb8fc9597
Solumath <solumath99@gmail.com>
2020-11-03 03:53:30 +0100

    cogs load and unload

---
+++ cogs/memes.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(command_prefix='?')
+
+class memes(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Ready!')
+        print('Logged in as', self.bot.user)
+        print('ID:', self.bot.user.id)
+
+    #uh oh reply
+    @commands.Cog.listener()                                          #actually @bot.event
+    async def on_message(self, message):
+        if message.author.bot:
+            if message.author.id == self.bot.user.id and \
+                message.content.startswith("<:") and \
+                message.content.endswith(">"):
+                 await message.channel.send(message.content)
+            return
+        elif "uh oh" in message.content:
+            await message.channel.send("uh oh")
+
+def setup(bot):
+    bot.add_cog(memes(bot))
