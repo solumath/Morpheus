fc26af02e4abd00a17b45d2a9aea9f8f002f4a57
Solumath <solumath99@gmail.com>
2021-08-02 19:19:06 +0200

    added tagrage command

--- cogs/memes.py
+++ cogs/memes.py
@@ -4,6 +4,8 @@ from discord_slash import cog_ext
 
 import aiohttp
 import random
+import asyncio
+import env
 
 class Memes(commands.Cog):
     def __init__(self, bot):
@@ -16,6 +18,22 @@ class Memes(commands.Cog):
     @cog_ext.cog_slash(name="nebudsalty", description="Nebud salty 'user'")
     async def nebudsalty(self, ctx, user = "<@624604891603795968>"):
         await ctx.send(f":salt: nebud salty {user}")
+    
+    @commands.cooldown(rate=1, per=100.0, type=commands.BucketType.user)
+    @commands.command()
+    async def tagrage(self, ctx, user: discord.Member, *text):
+        await ctx.message.delete()
+        for x in range(4):
+            await ctx.send(f"{user.mention} {' '.join(text)}")
+            await asyncio.sleep(15)
+    
+    @tagrage.error
+    async def mine_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            msg = 'Dostal jsi cooldown {:.2f}s'.format(error.retry_after)
+            await ctx.send(msg)
+        else:
+            raise error
 
 def setup(bot):
     bot.add_cog(Memes(bot))
