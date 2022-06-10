8b1342538873db32d7cdabcc77d34b082bd9dc5a
Solumath <solumath99@gmail.com>
2022-06-10 15:53:29 +0200

    add pet feature
    feature inspired by rubbergod FIT

---
+++ cogs/pet.py
@@ -0,0 +1,61 @@
+import disnake
+from disnake.ext import commands
+from config.messages import Messages
+
+from PIL import Image, ImageDraw
+
+from io import BytesIO
+import requests
+
+class Pet(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="pet", description=Messages.pet_brief)
+    async def pet(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User = None):
+        if user is None:
+            user = inter.author
+        if not user.avatar:
+            await inter.response.send_message(Messages.pet_unsupported_avatar)
+            return
+        url = user.display_avatar.with_format('jpg')
+        response = requests.get(url, timeout=10)
+        avatarFull = Image.open(BytesIO(response.content))
+
+        frames = []
+        deformWidth = [-1, -2, 1, 2, 1]
+        deformHeight = [4, 3, 1, 1, -4]
+        width = 80
+        height = 80
+
+        for i in range(5):
+            frame = Image.new('RGBA', (112, 112), (255, 255, 255, 1))
+            hand = Image.open(f"images/pet/{i}.png")
+            width = width - deformWidth[i]
+            height = height - deformHeight[i]
+            avatar = avatarFull.resize((width, height))
+            avatarMask = Image.new('1', avatar.size, 0)
+            draw = ImageDraw.Draw(avatarMask)
+            draw.ellipse((0, 0) + avatar.size, fill=255)
+            avatar.putalpha(avatarMask)
+
+            frame.paste(avatar, (112 - width, 112 - height), avatar)
+            frame.paste(hand, (0, 0), hand)
+            frames.append(frame)
+
+        with BytesIO() as image_binary:
+            frames[0].save(image_binary, format='GIF', save_all=True,
+                           append_images=frames[1:], duration=40,
+                           loop=0, transparency=0, disposal=2, optimize=False)
+            image_binary.seek(0)
+            await inter.response.send_message(file=disnake.File(fp=image_binary, filename="pet.gif"))
+
+    @pet.error
+    async def pet_error(self, inter: disnake.ApplicationCommandInteraction, error):
+        if isinstance(error, commands.MemberNotFound):
+            await inter.response.send_message(Messages.member_not_found.format(user=inter.author.id))
+            return True
+
+
+def setup(bot):
+    bot.add_cog(Pet(bot))
