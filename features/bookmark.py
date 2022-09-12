50df2145ce7adf2cde5b9a36effc78c25731449e
Solumath <solumath99@gmail.com>
2022-09-12 11:03:57 +0200

    update bookmark
    - bot cant send files bigger than 8MB so it will announce it to user
    - add button for original message link instead of hyperlink

--- features/bookmark.py
+++ features/bookmark.py
@@ -13,36 +13,44 @@ class BookmarkFeatures():
         if not title_name:
             title_name = Messages.bookmark_title.format(inter.guild.name)
 
-        embed = disnake.Embed(title=title_name, color=inter.message.author.colour)
-        embed.set_author(name=inter.message.author, icon_url=inter.message.author.avatar)
+        author = inter.message.author
+        embed = disnake.Embed(title=title_name, color=author.colour)
+        embed.set_author(name=author, icon_url=author.display_avatar.url)
         embed.set_image(image)
         embed.add_field(
             name="Channel",
-            value=f"[Jump to original message]({inter.message.jump_url}) in {inter.message.channel.mention}"
+            value=f"{inter.message.channel.mention}"
         )
         return embed
 
     async def create_bookmark_embed(self, inter, title_name=None):
         if not title_name:
             title_name = Messages.bookmark_title.format(inter.guild.name)
-        embed = disnake.Embed(title=title_name, colour=inter.message.author.colour)
-        embed.set_author(name=inter.message.author, icon_url=inter.message.author.avatar)
+
+        author = inter.message.author
+        embed = disnake.Embed(title=title_name, colour=author.colour)
+        embed.set_author(name=author, icon_url=author.display_avatar.url)
 
         content = ""
         if inter.message.embeds:
             for embed in inter.message.embeds:
-                embed.title, embed.colour = title_name, inter.message.author.colour
-                embed.set_author(name=inter.message.author, icon_url=inter.message.author.avatar)
+                embed.title, embed.colour = title_name, author.colour
+                embed.set_author(name=author, icon_url=author.display_avatar.url)
+
         if inter.message.content:
             content = inter.message.content
         else:
             content += "*Empty*"
 
         # create list of attachments
+        upload_limit = False
         images = []
         files_attached = []
         if inter.message.attachments:
             for attachment in inter.message.attachments:
+                if attachment.size > 8388608:
+                    upload_limit = True
+                    continue
                 if re.search(r"\.png|\.jpg|\.jpeg|\.gif$", str(attachment)):
                     images.append(attachment)
                 else:
@@ -51,14 +59,18 @@ class BookmarkFeatures():
         if images:
             embed.set_image(images[0])
             del images[0]
+
         if len(content) > 1024:
             parts = split(content, 1024)
             for msg in parts:
-                embed.add_field(name="Original message", value=msg, inline=False)
+                embed.add_field(name="Původní zpráva", value=msg, inline=False)
         else:
-            embed.add_field(name="Original message", value=content, inline=False)
+            embed.add_field(name="Původní zpráva", value=content, inline=False)
+
+        if upload_limit:
+            embed.add_field(name="Poznámka", value=Messages.bookmark_upload_limit, inline=False)
         embed.add_field(
             name="Channel",
-            value=f"[Jump to original message]({inter.message.jump_url}) in {inter.message.channel.mention}"
+            value=f"{inter.message.channel.mention}"
         )
         return ([embed], images, files_attached)
