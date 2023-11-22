833e420d37b6477c952fd9f30cc1afba3a717621
Juraj Martiček <marticekjuraj@gmail.com>
2023-11-22 10:37:51 +0100

    Updated gay leaderboard

--- cogs/gays.py
+++ cogs/gays.py
@@ -5,16 +5,36 @@ from disnake.channel import TextChannel
 
 
 mention_count = {}
+mention_author_count = {}
+
+def first_in_day(message, prev_message_day):
+    if prev_message_day is None:
+        return True
+    # WARN: Can be wrong if the next message is month after at the same day
+    return message.created_at.day != prev_message_day
 
 async def iterate_messages(channel):
     history = await channel.history(limit=None).flatten()
+    prev_message_day = None
     for message in history:
         if message.author.bot:
             continue
+
+        if not first_in_day(message, prev_message_day):
+            continue
+
+        if not len(message.mentions):
+            continue
+
         for mention in message.mentions:
             if mention not in mention_count:
                 mention_count[mention] = 0
             mention_count[mention] += 1
+
+        if message.author not in mention_author_count:
+            mention_author_count[message.author] = 0
+        mention_author_count[message.author] += 1
+        prev_message_day = message.created_at.day
 
 class Gays(commands.Cog):
     def __init__(self, bot):
@@ -22,21 +42,24 @@ class Gays(commands.Cog):
 
     @commands.slash_command(name="leadgay", description="Gay leaderboard")
     async def gays(self, inter: disnake.ApplicationCommandInteraction):
-        global mention_count
         await inter.response.defer()
-        for guild in self.bot.guilds:
-            if(guild.id != config.guild_id):
-                continue
-            for channel in guild.channels:
-                if(channel.id != config.bot_gay_channel):
-                    continue
-                if isinstance(channel, TextChannel):
-                    await iterate_messages(channel)
-        mention_count = dict(sorted(mention_count.items(), key=lambda item: item[1], reverse=True)[:10])
+        guild = self.bot.get_guild(config.guild_id)
+        channel = guild.get_channel(config.bot_gay_channel)
+        if isinstance(channel, TextChannel):
+            await iterate_messages(channel)
+       
+        mention_count_sorted = dict(sorted(mention_count.items(), key=lambda item: item[1], reverse=True)[:10])
         output = '# Naši nejlepší gejové:\n```'
-        for mention, count in mention_count.items():
+        for mention, count in mention_count_sorted.items():
+            output += f'{mention.name}: {count}\n'
+        output += '```\n'
+
+        mention_author_count_sorted = dict(sorted(mention_author_count.items(), key=lambda item: item[1], reverse=True)[:10])
+        output += '# Naši nejlepší tagři:\n```'
+        for mention, count in mention_author_count_sorted.items():
             output += f'{mention.name}: {count}\n'
         output += '```'
+
         await inter.edit_original_response(output)
 
 
