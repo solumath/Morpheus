6bfa3d082aedb44d5066840a235cf22add718af6
Solumath <solumath99@gmail.com>
2024-03-15 18:25:34 +0100

    add comments

--- custom/views.py
+++ custom/views.py
@@ -1,8 +1,22 @@
+from discord.ext import commands
+
 from cogs.bookmark.buttons import BookmarkView
 from cogs.roles.dropdowns import ChannelsSelectView, RolesSelectView
 
 
-def instantiate_views(bot):
+def instantiate_views(bot: commands.Bot) -> None:
+    """
+    Instantiate and add permanent views to the bot.
+
+    Parameters
+    ----------
+    bot: :clas:`commands.Bot`
+        The bot instance to which the views will be added.
+
+    Returns
+    -------
+    None
+    """
     views = {RolesSelectView, ChannelsSelectView, BookmarkView}
     for view in views:
         bot.add_view(view(bot))
