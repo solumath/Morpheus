cd6b7838d3a81e071c3e4f338e0bba732768d4af
Solumath <solumath99@gmail.com>
2024-05-08 09:24:27 +0200

    too long field

--- cogs/nasa/features.py
+++ cogs/nasa/features.py
@@ -7,9 +7,13 @@ def create_nasa_embed(response: dict) -> tuple[discord.Embed, str | None]:
     """
     Create embed for NASA API response
     """
-    embed = discord.Embed(title=response["title"], url=NasaMess.nasa_url, color=discord.Color.blurple())
+    embed = discord.Embed(
+        title=response["title"],
+        description=response["explanation"],
+        url=NasaMess.nasa_url,
+        color=discord.Color.blurple(),
+    )
     url = response["hdurl"] if response.get("hdurl", None) else response["url"]
     if response.get("media_type", None) != "video":
         embed.set_image(url=url)
-    embed.add_field(name="Explanation", value=response["explanation"], inline=False)
     return embed, url
