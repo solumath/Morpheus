9dce9c6093011b273d26a8c814278760da2b6a63
Solumath <solumath99@gmail.com>
2024-05-17 12:22:20 +0200

    fix custom error parent, use embed for ApiError

--- custom/custom_errors.py
+++ custom/custom_errors.py
@@ -1,24 +1,30 @@
+import discord
 from discord import app_commands
 from discord.ext import commands
 
 from custom.messages import CustomMess
 
 
-class InvalidTime(app_commands.CheckFailure, commands.CommandError):
+class InvalidTime(app_commands.AppCommandError, commands.CommandError):
     """An error indicating that the time format is invalid."""
 
     def __init__(self, time_format: str) -> None:
         self.message = CustomMess.invalid_time_format(time_format=time_format)
 
 
-class ApiError(app_commands.CheckFailure, commands.CommandError):
+class ApiError(app_commands.AppCommandError, commands.CommandError):
     """An error indicating that the api returned invalid status."""
 
     def __init__(self, error: str) -> None:
-        self.message = CustomMess.api_error(error=error)
+        embed = discord.Embed(
+            title="API Error",
+            description=CustomMess.api_error(error=error),
+            color=discord.Color.red(),
+        )
+        self.embed = embed
 
 
-class NotAdminError(app_commands.CheckFailure, commands.CommandError):
+class NotAdminError(app_commands.AppCommandError, commands.CommandError):
     """An error indicating that a user doesn't have permissions to use
     a command that is available only to admins of bot.
     """
