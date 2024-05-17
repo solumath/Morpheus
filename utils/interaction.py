fea926e00aa45b9105b93530fdd70f031bba7b29
Solumath <solumath99@gmail.com>
2024-05-17 12:17:14 +0200

    update custom send with embed

--- utils/interaction.py
+++ utils/interaction.py
@@ -2,12 +2,17 @@ import discord
 
 
 async def custom_send(
-    inter: discord.Interaction, content: str, ephemeral: bool = False, edit: bool = True, **kwargs
+    inter: discord.Interaction,
+    content: str = None,
+    ephemeral: bool = False,
+    embed: discord.Embed = None,
+    edit: bool = True,
+    **kwargs,
 ) -> None:
     """Send a message for the interaction or edit the original response if it's already done."""
     if inter.response.is_done() and edit:
-        await inter.edit_original_response(content=content, **kwargs)
+        await inter.edit_original_response(content=content, embed=embed, **kwargs)
     elif inter.response.is_done():
-        await inter.followup.send(content=content, ephemeral=ephemeral, **kwargs)
+        await inter.followup.send(content=content, embed=embed, ephemeral=ephemeral, **kwargs)
     else:
-        await inter.response.send_message(content=content, ephemeral=ephemeral, **kwargs)
+        await inter.response.send_message(content=content, embed=embed, ephemeral=ephemeral, **kwargs)
